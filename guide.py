5a42053f1d2453795c26e51152b8a50f4e86f7e8
podomin <82488911+podomin@users.noreply.github.com>
2023-12-24 18:00:17 +0900

    Add files via upload

---
+++ guide.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+
+
+st.title("🍄독버섯이 말아준다")
+st.subheader("데이터로 말아주는 생리용품 가이드")
+st.sidebar.markdown("guide")
+
+st.warning("해당 데이터는 2023년 12월 20일 기준으로 수집한 데이터입니다")
+st.warning("'생리대' 범주에서 인기순 top 20 제품과 '탐폰 & 생리컵' 범주에서의 \
+        모든 제품의 리뷰 데이터를 분석한 자료임을 참고해주세요")
+
+st.markdown("라떼…는 생리대 브랜드도 별로 없고 ~ 사실 그냥 내가 필요해서 내가 만든다")
+
+st.sidebar.markdown("🍄WHAT IS 독버섯?")
+st.sidebar.markdown(
+    "그냥 프로젝트성 팀인디요...🥸"  
+    )    
+
+
---
+++ pages/EDA.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+
+st.sidebar.markdown("EDA")
+
+st.title("데이터 분석으로 살펴본 생리용품")
+
+tab_explore, tab_analysis = st.tabs(["길을", "잃었다"])          
+
+with tab_explore:
+    
+    st.markdown("### 길을 잃은 독버섯 ")
+
+st.sidebar.markdown("🍄WHAT IS 독버섯?")
+st.sidebar.markdown(
+    "그냥 프로젝트성 팀인디요...🥸"  
+    )    
